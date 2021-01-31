ce61f91bdd780fb44215e548899b6542301fe311
jguillevic <guillevicjerome@yahoo.fr>
2021-01-31 18:31:45 +0100

    Modifs de l'autoload pour fonctionner sour Linux

--- AutoLoad.php
+++ AutoLoad.php
@@ -2,7 +2,9 @@
 require_once join(DIRECTORY_SEPARATOR,[ __DIR__, 'vendor', 'autoload.php' ]);
 
 spl_autoload_register(function($className) {
-	$file = $className . '.php';
+	$classNameItems = explode('\\', $className);
+	$file = join(DIRECTORY_SEPARATOR, $classNameItems) . '.php';
+
 	if (file_exists($file)) {
 		require_once $file;
     }
